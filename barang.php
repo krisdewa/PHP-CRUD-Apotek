669f0bd05a773e1ed165befa425f1495ac8dc824
NoobMaster001 <56239834+NaufalHafizh@users.noreply.github.com>
2021-06-26 18:51:12 +0700

    Update 80% 26 Juni 2021

--- barang.php
+++ barang.php
@@ -29,52 +29,111 @@
 </head>
 
 <body>
+    <div class="wrap-tab">
         <div class="tab">
-            <table class="table-tab">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">ID Barang</th>
-                        <th scope="col">Nama Barang</th>
-                        <th scope="col">Kategori Barang</th>
-                        <th scope="col">Harga Jual</th>
-                        <th scope="col">Harga Beli</th>
-                        <th scope="col">Stok</th>
-                        <th scope="col">Edit</th>
-                        <th scope="col">Delete</th>
-                    </tr>
-                </thead>
+            <div style="background-color: #E3A2AD;" class="card">
+                <div style="background-color: #E3A2AD;" class="border-0 card-header">
+                    <h5 class="text-light card-title">Data Staff</h5>
+                </div>
+                <div class="card-body"> 
+                    <table class="table-tab">
+                        <thead>
+                            <tr style="color: #E3A2AD;">
+                                <th scope="col">No</th>
+                                <th scope="col">ID Barang</th>
+                                <th scope="col">Nama Barang</th>
+                                <th scope="col">Kategori Barang</th>
+                                <th scope="col">Harga Jual</th>
+                                <th scope="col">Harga Beli</th>
+                                <th scope="col">Stok</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
 
-                <?php
-                    $no =1;
-                    foreach($results as $result) :
-                ?>
+                        <?php
+                            $no =1;
+                            foreach($results as $result) :
+                        ?>
 
-                <tbody>
-                    <tr>
-                        <td> <?= $no ?></td>
-                        <td> <?= $result['ID_Barang'] ?></td>
-                        <td> <?= $result['nama_barang'] ?></td>
-                        <td> <?= $result['kategori'] ?></td>
-                        <td> <?= $result['harga_jual'] ?></td>
-                        <td> <?= $result['harga_beli'] ?></td>
-                        <td> <?= $result['stok'] ?></td>
-                        <td>
-                            <a class="btn btn-warning" href="edit_barang.php?ID_Barang=<?=$result['ID_Barang'];?>"><i class="bi bi-pencil"></i></a>
-                        </td>
-                        <td>
-                            <a class="btn btn-danger" href="hapus_barang.php?ID_Barang=<?=$result['ID_Barang'];?>"><i class="bi bi-trash"></i></a>
-                        </td>
-                    </tr>
+                        <tbody>
+                            <tr>
+                                <td> <?= $no ?></td>
+                                <td> <?= $result['ID_Barang'] ?></td>
+                                <td> <?= $result['nama_barang'] ?></td>
+                                <td> <?= $result['kategori'] ?></td>
+                                <td> <?= $result['harga_jual'] ?></td>
+                                <td> <?= $result['harga_beli'] ?></td>
+                                <td> <?= $result['stok'] ?></td>
+                                <td>
+                                    <a class="btn btn-warning" href="edit_barang.php?ID_Barang=<?=$result['ID_Barang'];?>"><i class="bi bi-pencil"></i></a>
+                                </td>
+                                <td>
+                                    <a class="btn btn-danger" href="hapus_barang.php?ID_Barang=<?=$result['ID_Barang'];?>"><i class="bi bi-trash"></i></a>
+                                </td>
+                            </tr>
 
-                </tbody>
+                        </tbody>
 
-                <?php
-                    $no++;
-                    endforeach;
-                ?>
+                        <?php
+                            $no++;
+                            endforeach;
+                        ?>
 
-            </table>
+                    </table>
+                    <div class="bawah">
+                        <button style="margin-left: 10px" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#tambahsupliyer">Tambah Barang</button>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="tambahsupliyer" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div style="background-color: #E9A2AD;" class="modal-content">
+        <div class="border-0 modal-header">
+            <h5 class="text-light fw-bolder modal-title" id="exampleModalLabel">APTK24</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+            <h5 style="color: black; margin-top: 8px;" class="text-light fw-bolder text-center">TAMBAH BARANG</h5>
+        <div class="modal-body">
+            <form action="">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="tambahid">ID Barang</label>
+                        <input type="text" class="form-control" id="IDBarang" placeholder=""> <br>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="tambahnama">Nama Barang</label>
+                        <input type="text" class="form-control" id="NamaBarang" placeholder="">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="tambahjeniskel">Kategori Barang</label>
+                        <input type="text" class="form-control" id="KategoriBarang" placeholder=""> <br>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="inputPassword4">Harga Jual</label>
+                        <input type="text" class="form-control" id="HargaJual" placeholder="">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="inputPassword4">Harga Beli</label>
+                        <input type="text" class="form-control" id="HargaBeli" placeholder="">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="inputPassword4">Stok</label>
+                        <input type="text" class="form-control" id="Stok" placeholder="">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="border-0 modal-footer">
+            <button style="margin-top: 50px; margin-bottom: 50px;" type="button" class="btn btn-danger" data-bs-dismiss="modal">Hapus</button>
+            <button style="margin-top: 50px; margin-bottom: 50px; margin-right: 7px;" type="submit" class="btn btn-success">Simpan</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    <!-- end modal -->
 </body>
 </html>
--- supplier.php
+++ supplier.php
@@ -16,9 +16,6 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
-
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,48 +25,99 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </head>
 <body>
+    <div class="wrap-tab">
         <div class="tab">
-            <table class="table-tab">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">ID Supplier</th>
-                        <th scope="col">Nama Supplier</th>
-                        <th scope="col">Alamat Supplier</th>
-                        <th scope="col">No Telp</>
-                        <th scope="col">Edit</th>
-                        <th scope="col">Delete</th>
-                    </tr>
-                </thead>
+            <div style="background-color: #E3A2AD;" class="card">
+            <div style="background-color: #E3A2AD;" class="border-0 card-header">
+                    <h5 class="text-light card-title">Data Supplier</h5>
+                </div>
+                <div class="card-body">
+                    <table class="table-tab">
+                        <thead>
+                            <tr style="color: #E3A2AD;" >
+                                <th scope="col">No</th>
+                                <th scope="col">ID Supplier</th>
+                                <th scope="col">Nama Supplier</th>
+                                <th scope="col">Alamat Supplier</th>
+                                <th scope="col">No Telp</>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
 
-                <?php
-                    $no =1;
-                    foreach($results as $result) :
-                ?>
-                
-                <tbody>
-                    <tr>
-                        <td> <?= $no ?></td>
-                        <td> <?= $result['ID_Supplier'] ?></td>
-                        <td> <?= $result['nama_supplier'] ?></td>
-                        <td> <?= $result['alamat_supplier'] ?></td>
-                        <td> <?= $result['No_Telp'] ?></td>
-                        <td>
-                            <a class="btn btn-warning" href="edit_supplier.php?ID_Supplier=<?=$result['ID_Supplier'];?>"><i class="bi bi-pencil"></i></a>
-                        </td>
-                        <td>
-                            <a class="btn btn-danger" href="hapus_supplier.php?ID_Supplier=<?=$result['ID_Supplier'];?>"><i class="bi bi-trash"></i></a>
-                        </td>
-                    </tr>
+                        <?php
+                            $no =1;
+                            foreach($results as $result) :
+                        ?>
                         
-                </tbody>
+                        <tbody>
+                            <tr>
+                                <td> <?= $no ?></td>
+                                <td> <?= $result['ID_Supplier'] ?></td>
+                                <td> <?= $result['nama_supplier'] ?></td>
+                                <td> <?= $result['alamat_supplier'] ?></td>
+                                <td> <?= $result['No_Telp'] ?></td>
+                                <td>
+                                    <a class="btn btn-warning" href="edit_supplier.php?ID_Supplier=<?=$result['ID_Supplier'];?>"><i class="bi bi-pencil"></i></a>
+                                </td>
+                                <td>
+                                    <a class="btn btn-danger" href="hapus_supplier.php?ID_Supplier=<?=$result['ID_Supplier'];?>"><i class="bi bi-trash"></i></a>
+                                </td>
+                            </tr>
+                                
+                        </tbody>
 
-                <?php
-                    $no++;
-                    endforeach;
-                ?>
+                        <?php
+                            $no++;
+                            endforeach;
+                        ?>
 
-            </table>
+                    </table>
+                    <div class="bawah">
+                        <button style="margin-left: 10px" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#tambahsupliyer">Tambah Supplier</button>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="tambahsupliyer" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div style="background-color: #E9A2AD;" class="modal-content">
+        <div class="border-0 modal-header">
+            <h5 class="text-light fw-bolder modal-title" id="exampleModalLabel">APTK24</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+            <h5 style="color: black; margin-top: 8px;" class="text-light fw-bolder text-center">TAMBAH SUPPLIER</h5>
+        <div class="modal-body">
+            <form action="">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="tambahid">ID Supplier</label>
+                        <input type="text" class="form-control" id="IDSupplier" placeholder=""> <br>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="tambahnama">Nama Supplier</label>
+                        <input type="text" class="form-control" id="NamaSupplier" placeholder="">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="tambahjeniskel">Alamat Supplier</label>
+                        <input type="text" class="form-control" id="AlamatSupplier" placeholder="">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label style="margin-bottom: 5px;" for="inputPassword4">No Telp</label>
+                        <input type="text" class="form-control" id="NoTelp" placeholder="">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="border-0 modal-footer">
+            <button style="margin-top: 50px; margin-bottom: 50px;" type="button" class="btn btn-danger" data-bs-dismiss="modal">Hapus</button>
+            <button style="margin-top: 50px; margin-bottom: 50px; margin-right: 7px;" type="submit" class="btn btn-success">Simpan</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    <!-- end modal -->
 </body>
 </html>
