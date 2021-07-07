19735bcb198ad165ecf3bd51b4a562ba2280a0f8
krisdewa <59174531+krisdewa@users.noreply.github.com>
2021-07-07 11:24:17 +0700

    fix minor

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 PRPL Project Apotek
-Tugas Akhir PRPL Semester 4 Universitas Ahmad Dahlan
-
-Kontributor :
-Krisna Dewa Pratama
-Gentha Muhamad Djamal
-Muhammad Naufal Hafizh
-Lalu Gugus Aulia Rahman 
+ 
---
+++ hapus_staff.php
@@ -0,0 +1,10 @@
+<?php
+    require_once("connectdb.php");
+    
+    $ID_Pegawai = $_GET['ID_Pegawai'];
+
+    $sql_delete = "DELETE FROM staff WHERE ID_Pegawai = '$ID_Pegawai' ";
+    mysqli_query($koneksi, $sql_delete);
+
+    header("Location:staff.php");
+?>
