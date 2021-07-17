4e0c7505f19324561c4d496e2b84400292b65136
genthadjamal <68898618+genthadjamal@users.noreply.github.com>
2021-07-17 17:22:29 +0700

    Update navbar-staff.php

--- navbar-staff.php
+++ navbar-staff.php
@@ -13,7 +13,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #7ECA9C;">
     <div class="container">
-        <a class="navbar-brand" href="halaman-staff.php" style="font-size: 40px;">APOTEK
+        <a class="navbar-brand" href="halaman-staff.php" style="font-size: 40px; font-family: 'Assistant', sans-serif; font-weight: bold;">APTK24
             <!-- <img src="" alt="" class="src"> -->
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,16 +23,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item mx-3" style="font-size: 20px;">
-                    <a <?php if($pageactive == "home") echo "class='nav-link active'";?> class="nav-link" aria-current="page" href="halaman-staff.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href="halaman-staff.php">Home</a>
                 </li>
                 <li class="nav-item mx-3" style="font-size: 20px;">
-                    <a <?php if($pageactive == "barang") echo "class='nav-link active'";?> class="nav-link" aria-current="page" href="data-staff-barang.php">Barang</a>
+                    <a class="nav-link active" aria-current="page" href="data-staff-barang.php">Barang</a>
                 </li>
                 <li class="nav-item mx-3" style="font-size: 20px;">
-                    <a <?php if($pageactive == "service") echo "class='nav-link active'";?> class="nav-link" aria-current="page" href="staff-service.php">Service</a>
+                    <a class="nav-link active" aria-current="page" href="staff-service.php">Service</a>
                 </li>
                 <li>
-                <a id="Logoutbtn" class="btn btn-primary nav-link active" aria-current="page" href="logout.php" onclick="return confirm('Yakin ingin logout ?')">LOGOUT</a>
+                <a id="Logoutbtn" class="btn btn-primary nav-link active" aria-current="page" href="logout.php" style="font-size: 21px;font-family: 'Assistant', sans-serif; font-weight: 600; letter-spacing: 1px" onclick="return confirm('Yakin ingin logout ?')">Logout</a>
                 </li>
             </ul>
         </div>
